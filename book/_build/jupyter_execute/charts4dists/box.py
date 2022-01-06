2fc60d427e12d592c665e4f19a29465720c1ba71
kakeami <kakeami.contact@gmail.com>
2022-01-06 11:21:38 +0900

    分布系の作図例を更新

--- book/_build/jupyter_execute/charts4dists/box.py
+++ book/_build/jupyter_execute/charts4dists/box.py
@@ -3,6 +3,51 @@
 
 # # Box plot
 
+# ## 概要
+
+# ## Plotlyによる作図方法
+
+# ## MADB Labを用いた作図例
+
+# ### 下準備
+
+# In[1]:
+
+
+import pandas as pd
+import plotly.express as px
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# In[2]:
+
+
+# 前処理の結果，以下に分析対象ファイルが格納されていることを想定
+PATH_DATA = '../../data/preprocess/out/magazines.csv'
+# Jupyter Book用のPlotlyのrenderer
+RENDERER = 'plotly_mimetype+notebook'
+
+
+# In[3]:
+
+
+def show_fig(fig):
+    """Jupyter Bookでも表示可能なようRendererを指定"""
+    fig.show(renderer=RENDERER)
+
+
+# In[5]:
+
+
+df = pd.read_csv(PATH_DATA)
+
+
+# ### 作品別の合計連載週数 
+
+# ### 作者別の合計連載週数
+
 # In[ ]:
 
 
