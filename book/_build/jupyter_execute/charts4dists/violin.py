8dc2ae3bca5b311d0cf8ef4ea2ff486433a16a52
kakeami <kakeami.contact@gmail.com>
2022-01-10 14:30:31 +0900

    Violinの描画範囲の修正を完了

--- book/_build/jupyter_execute/charts4dists/violin.py
+++ book/_build/jupyter_execute/charts4dists/violin.py
@@ -25,7 +25,7 @@
 
 # ### 下準備
 
-# In[1]:
+# In[8]:
 
 
 import pandas as pd
@@ -35,7 +35,7 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
-# In[2]:
+# In[9]:
 
 
 # 前処理の結果，以下に分析対象ファイルが格納されていることを想定
@@ -44,7 +44,7 @@ PATH_DATA = '../../data/preprocess/out/magazines.csv'
 RENDERER = 'plotly_mimetype+notebook'
 
 
-# In[3]:
+# In[10]:
 
 
 def show_fig(fig):
@@ -52,7 +52,7 @@ def show_fig(fig):
     fig.show(renderer=RENDERER)
 
 
-# In[4]:
+# In[19]:
 
 
 df = pd.read_csv(PATH_DATA)
@@ -60,20 +60,54 @@ df = pd.read_csv(PATH_DATA)
 
 # ### 雑誌別・作品別の合計連載週数
 
-# In[5]:
+# In[22]:
 
 
 df_plot =     df.value_counts(['mcname', 'cname']).reset_index(name='weeks')
-fig = px.violin(df_plot, x='mcname', y='weeks', title='雑誌別・作品別の合計連載週数')
+# X軸の表示順を調整
+df_plot = df_plot.sort_values('mcname', ignore_index=True)
+fig = px.violin(
+    df_plot, x='mcname', y='weeks', 
+    title='雑誌別・作品別の合計連載週数')
+show_fig(fig)
+
+
+# In[23]:
+
+
+fig = px.violin(
+    df_plot, x='mcname', y='weeks', 
+    title='雑誌別・作品別の合計連載週数')
+fig.update_yaxes(range=[0, 200])
 show_fig(fig)
 
 
 # ### 雑誌別・作者別の合計連載週数
 
-# In[6]:
+# In[24]:
 
 
 df_plot =     df.value_counts(['mcname', 'creator']).reset_index(name='weeks')
-fig = px.violin(df_plot, x='mcname', y='weeks', title='雑誌別・作者別の合計連載週数')
+# X軸の表示順を調整
+df_plot = df_plot.sort_values('mcname', ignore_index=True)
+fig = px.violin(
+    df_plot, x='mcname', y='weeks', 
+    title='雑誌別・作者別の合計連載週数')
 show_fig(fig)
+
+
+# In[25]:
+
+
+fig = px.violin(
+    df_plot, x='mcname', y='weeks', 
+    title='雑誌別・作者別の合計連載週数')
+fig.update_yaxes(range=[0, 200])
+show_fig(fig)
+
+
+# In[ ]:
+
+
+
 
