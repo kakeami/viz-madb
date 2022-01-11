df4ad7477d8912244bd7a384dbd84e9e0434fec9
kakeami <kakeami.contact@gmail.com>
2022-01-11 18:16:18 +0900

    Pie chartを追加

--- book/_build/jupyter_execute/charts4props/pie.py
+++ book/_build/jupyter_execute/charts4props/pie.py
@@ -9,8 +9,61 @@
 
 # ## MADB Labを用いた作図例
 
-# In[ ]:
+# ### 下準備
+
+# In[1]:
 
 
+import pandas as pd
+import plotly.express as px
 
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# In[2]:
+
+
+# 前処理の結果，以下に分析対象ファイルが格納されていることを想定
+PATH_DATA = '../../data/preprocess/out/magazines.csv'
+# Jupyter Book用のPlotlyのrenderer
+RENDERER = 'plotly_mimetype+notebook'
+
+
+# In[3]:
+
+
+def show_fig(fig):
+    """Jupyter Bookでも表示可能なようRendererを指定"""
+    fig.show(renderer=RENDERER)
+
+
+# In[4]:
+
+
+df = pd.read_csv(PATH_DATA)
+
+
+# ### 雑誌別の合計作品数
+
+# In[13]:
+
+
+df_plot =     df.groupby('mcname')['cname'].nunique().reset_index()
+fig = px.pie(
+    df_plot, values='cname', names='mcname',
+    title='雑誌別の合計作品数')
+show_fig(fig)
+
+
+# ### 雑誌別の合計作者数
+
+# In[14]:
+
+
+df_plot =     df.groupby('mcname')['creator'].nunique().reset_index()
+fig = px.pie(
+    df_plot, values='creator', names='mcname',
+    title='雑誌別の合計作者数')
+show_fig(fig)
 
