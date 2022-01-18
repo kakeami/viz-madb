bf5681491b2c8f8c9f6168e68a73907d09b673be
kakeami <kakeami.contact@gmail.com>
2022-01-18 16:24:24 +0900

    Minor

--- book/_build/jupyter_execute/charts4dists/density.py
+++ book/_build/jupyter_execute/charts4dists/density.py
@@ -21,9 +21,9 @@
 # 上記の例では，`df`の`col_x`列をX軸，その確率密度をY軸にとったDensity plotのオブジェクト`fig`を作成します．
 # ただし，`label`のように凡例名を指定する必要があることにご注意ください．
 
-# ```{admonition} `show_hist=False`
+# :::{admonition} `show_hist=False`
 # `plotly.figure_factory.create_distplot()`はデフォルト設定でヒストグラムとDensity plotの両方を作図します．Density plotのみ表示したい場合は，`show_hist=False`を指定しましょう．
-# ```
+# :::
 
 # ## MADB Labを用いた作図例
 
