c86c3e7861920a46d781c207fc355370c3ad229c
kakeami <kakeami.contact@gmail.com>
2022-01-20 03:15:13 +0900

    密度プロットを追加

--- book/_build/jupyter_execute/charts4assocs/countours.py
+++ book/_build/jupyter_execute/charts4assocs/countours.py
@@ -11,6 +11,101 @@
 
 # ### 下準備
 
+# In[1]:
+
+
+import pandas as pd
+import plotly.express as px
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# In[2]:
+
+
+# 前処理の結果，以下に分析対象ファイルが格納されていることを想定
+PATH_DATA = '../../data/preprocess/out/episodes.csv'
+# Jupyter Book用のPlotlyのrenderer
+RENDERER = 'plotly_mimetype+notebook'
+
+
+# In[3]:
+
+
+# 連載週数の最小値
+MIN_WEEKS = 5
+
+
+# In[4]:
+
+
+def show_fig(fig):
+    """Jupyter Bookでも表示可能なようRendererを指定"""
+    fig.update_layout(margin=dict(t=50, l=25, r=25, b=25))
+    fig.show(renderer=RENDERER)
+
+
+# In[5]:
+
+
+df = pd.read_csv(PATH_DATA)
+
+
+# ### 作品別の平均掲載位置と連載週数
+
+# In[6]:
+
+
+df_plot =     df.groupby('cname')['pageStartPosition'].    agg(['count', 'mean']).reset_index()
+df_plot.columns = ['cname', 'weeks', 'position']
+df_plot =     df_plot[df_plot['weeks'] >= MIN_WEEKS].reset_index(drop=True)
+
+
+# In[13]:
+
+
+fig = px.density_contour(
+    df_plot, x='position', y='weeks',)
+show_fig(fig)
+
+
+# In[14]:
+
+
+fig.update_yaxes(range=[0, 200])
+show_fig(fig)
+
+
+# In[15]:
+
+
+fig.update_traces(
+    contours_coloring="fill", 
+    contours_showlabels = True)
+show_fig(fig)
+
+
+# ### 雑誌別・作品別の平均掲載位置と連載週数
+
+# In[16]:
+
+
+df_plot =     df.groupby(['mcname', 'cname'])['pageStartPosition'].    agg(['count', 'mean']).reset_index()
+df_plot.columns = ['mcname', 'cname', 'weeks', 'position']
+df_plot =     df_plot[df_plot['weeks'] >= MIN_WEEKS].reset_index(drop=True)
+
+
+# In[18]:
+
+
+fig = px.density_contour(
+    df_plot, x='position', y='weeks',
+    color='mcname')
+fig.update_yaxes(range=[0, 200])
+show_fig(fig)
+
+
 # In[ ]:
 
 
