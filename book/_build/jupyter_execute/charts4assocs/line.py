59b8eb370cf1413d665e9d0cc1d5d553129000bf
kakeami <kakeami.contact@gmail.com>
2022-01-21 17:38:00 +0900

    折れ線グラフを修正

--- book/_build/jupyter_execute/charts4assocs/line.py
+++ book/_build/jupyter_execute/charts4assocs/line.py
@@ -21,7 +21,7 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
-# In[3]:
+# In[2]:
 
 
 # 前処理の結果，以下に分析対象ファイルが格納されていることを想定
@@ -30,16 +30,14 @@ PATH_DATA = '../../data/preprocess/out/episodes.csv'
 RENDERER = 'plotly_mimetype+notebook'
 
 
-# In[54]:
+# In[5]:
 
 
 # 連載週数の最小値
 MIN_WEEKS = 5
-# 抽出するマンガ作品数
-N_CNAMES = 4
 
 
-# In[55]:
+# In[6]:
 
 
 def show_fig(fig):
@@ -53,79 +51,45 @@ def show_fig(fig):
     fig.show(renderer=RENDERER)
 
 
-# In[56]:
+# In[7]:
 
 
 df = pd.read_csv(PATH_DATA)
 
 
-# ### エピソードの掲載日と掲載位置
+# ### 長期連載作品の掲載位置の推移
 
-# In[98]:
+# In[8]:
 
 
-mcnames = sorted(df['mcname'].unique())
-for mcname in mcnames:
-    df_tmp = df[df['mcname']==mcname].reset_index(drop=True)
-    df_cname =         df_tmp.value_counts('cname').reset_index(name='weeks')
-    df_cname =         df_cname.sort_values(
-            'weeks', ascending=False, ignore_index=True)
-    cnames = df_cname['cname'][:N_CNAMES].values
-    df_plot = df_tmp[df_tmp['cname'].isin(cnames)].        reset_index(drop=True)
-    df_plot = df_plot.rename(columns={
-        'cname': '作品名',
-        'datePublished': '掲載日',
-        'pageStartPosition': '掲載位置',
-        'epname': '話名'})
+df_tmp =     df.groupby('cname')['pageStartPosition']    .agg(['count', 'mean']).reset_index()
+df_tmp =     df_tmp.sort_values('count', ascending=False, ignore_index=True)    .head(10)
+cname2position = df_tmp.groupby('cname')['mean'].first().to_dict()
+
+
+# In[10]:
+
+
+df_plot = df[df['cname'].isin(list(cname2position.keys()))]    .reset_index(drop=True)
+df_plot['position'] = df_plot['cname'].apply(
+    lambda x: cname2position[x])
+df_plot['datePublished'] = pd.to_datetime(df_plot['datePublished'])
+df_plot =     df_plot.sort_values(['position', 'datePublished'], ignore_index=True)
+
+
+# In[19]:
+
+
+cnames = df_plot['cname'].unique()
+for cname in cnames:
+    df_c = df_plot[df_plot['cname']==cname].reset_index(drop=True)
     fig = px.line(
-        df_plot, x='掲載日', y='掲載位置',
-        color='作品名', title=f'{mcname}の長期連載作品',
-        hover_data=['話名'], height=500)
-    fig.update_layout(hovermode='x unified')
-    show_fig(fig)
-
-
-# ### エピソードの話数と掲載位置
-
-# In[75]:
-
-
-def add_epid_to_df(df):
-    """dfのcnameごとに1から順番に話数を追加"""
-    cnames = df['cname'].unique()
-    df_out = pd.DataFrame()
-    
-    for cname in cnames:
-        df_tmp = df[df['cname']==cname].reset_index(drop=True)
-        df_tmp['epid'] = df_tmp.index + 1
-        df_out = pd.concat([df_out, df_tmp], ignore_index=True)
-        
-    return df_out
-
-
-# In[99]:
-
-
-mcnames = sorted(df['mcname'].unique())
-for mcname in mcnames:
-    df_tmp = df[df['mcname']==mcname].reset_index(drop=True)
-    df_cname =         df_tmp.value_counts('cname').reset_index(name='weeks')
-    df_cname =         df_cname.sort_values(
-            'weeks', ascending=False, ignore_index=True)
-    cnames = df_cname['cname'][:N_CNAMES].values
-    df_plot = df_tmp[df_tmp['cname'].isin(cnames)].        reset_index(drop=True)
-    df_plot = add_epid_to_df(df_plot)
-    df_plot = df_plot.rename(columns={
-        'cname': '作品名',
-        'epname': '話名',
-        'epid': '話数',
-        'pageStartPosition': '掲載位置',})
-    fig = px.line(
-        df_plot, x='話数', y='掲載位置',
-        facet_col='作品名', facet_col_wrap=2,
-        title=f'{mcname}の長期連載作品',
-        hover_data=['話名'], height=500)
-    fig.update_layout(
-        hovermode='x unified')
+        df_c, x='datePublished', y='pageStartPosition',
+        hover_data=['epname'], title=f'{cname}の掲載位置の推移')
+    fig.update_traces(
+        mode='lines+markers',
+        marker=dict(line_width=1, size=10))
+    fig.update_xaxes(title='発売日')
+    fig.update_yaxes(title='掲載位置')
     show_fig(fig)
 
