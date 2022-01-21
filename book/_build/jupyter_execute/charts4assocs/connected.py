08df8fc701512e8879c50b8eeb64b74b88f064e0
kakeami <kakeami.contact@gmail.com>
2022-01-21 17:49:14 +0900

    折れ線グラフを修正

--- book/_build/jupyter_execute/charts4assocs/connected.py
+++ book/_build/jupyter_execute/charts4assocs/connected.py
@@ -30,7 +30,14 @@ PATH_DATA = '../../data/preprocess/out/episodes.csv'
 RENDERER = 'plotly_mimetype+notebook'
 
 
-# In[4]:
+# In[9]:
+
+
+UNIT_YEARS = 5
+YEARS_TO_DROP = ['1970', '2015']
+
+
+# In[3]:
 
 
 def add_years_to_df(df, unit_years=10):
@@ -41,7 +48,7 @@ def add_years_to_df(df, unit_years=10):
     return df_new
 
 
-# In[5]:
+# In[4]:
 
 
 def show_fig(fig):
@@ -55,7 +62,7 @@ def show_fig(fig):
     fig.show(renderer=RENDERER)
 
 
-# In[6]:
+# In[5]:
 
 
 df = pd.read_csv(PATH_DATA)
@@ -63,33 +70,29 @@ df = pd.read_csv(PATH_DATA)
 
 # ### 作品数と作家数の推移
 
-# In[77]:
+# In[17]:
 
 
-YEARS_TO_DROP = ['1970', '2015']
-
-
-# In[78]:
-
-
-df = add_years_to_df(df, 5)
+df = add_years_to_df(df, UNIT_YEARS)
 df_plot =     df.groupby('years')['cname', 'creator'].nunique().reset_index()
 # 最初と最後の年度は不十分なデータなので除外
 df_plot =     df_plot[~df_plot['years'].isin(YEARS_TO_DROP)]    .reset_index(drop=True)
 
 
-# In[79]:
+# In[18]:
 
 
 fig = px.line(
-    df_plot, x='cname', y='creator', text='years')
-fig.update_traces(textposition='bottom right')
+    df_plot, x='cname', y='creator', text='years',)
+fig.update_traces(
+    marker={'size': 15, 'line_width':1, 'opacity':0.8},
+    textposition='bottom right')
 show_fig(fig)
 
 
 # ### 雑誌別の作品数と作家数の推移
 
-# In[85]:
+# In[19]:
 
 
 df = add_years_to_df(df, 5)
@@ -98,18 +101,17 @@ df_plot =     df.groupby(['mcname', 'years'])['cname', 'creator']    .nunique().
 df_plot =     df_plot[~df_plot['years'].isin(YEARS_TO_DROP)]    .reset_index(drop=True)
 
 
-# In[88]:
+# In[23]:
 
 
 fig = px.line(
     df_plot, x='cname', y='creator', text='years',
-    facet_col='mcname', facet_col_wrap=2)
-fig.update_traces(textposition='bottom right')
+    facet_col='mcname', facet_col_wrap=2,
+    height=600)
+fig.update_traces(
+    marker={'size': 15, 'line_width':1, 'opacity':0.8},
+    textposition='bottom right')
+fig.for_each_annotation(
+    lambda a: a.update(text=a.text.split("=")[-1]))
 show_fig(fig)
-
-
-# In[ ]:
-
-
-
 
