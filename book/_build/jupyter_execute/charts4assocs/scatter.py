dd1c36f1e5a0fca2fa86ec6ccebf5d323ad3d83c
kakeami <kakeami.contact@gmail.com>
2022-01-21 17:12:22 +0900

    散布図を修正

--- book/_build/jupyter_execute/charts4assocs/scatter.py
+++ book/_build/jupyter_execute/charts4assocs/scatter.py
@@ -21,7 +21,7 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
-# In[5]:
+# In[2]:
 
 
 # 前処理の結果，以下に分析対象ファイルが格納されていることを想定
@@ -30,14 +30,14 @@ PATH_DATA = '../../data/preprocess/out/episodes.csv'
 RENDERER = 'plotly_mimetype+notebook'
 
 
-# In[36]:
+# In[3]:
 
 
 # 連載週数の最小値
 MIN_WEEKS = 5
 
 
-# In[63]:
+# In[4]:
 
 
 def show_fig(fig):
@@ -50,7 +50,7 @@ def show_fig(fig):
     fig.show(renderer=RENDERER)
 
 
-# In[7]:
+# In[5]:
 
 
 df = pd.read_csv(PATH_DATA)
@@ -58,7 +58,7 @@ df = pd.read_csv(PATH_DATA)
 
 # ### 作品別の平均掲載位置と連載週数
 
-# In[64]:
+# In[21]:
 
 
 df_plot =     df.groupby('cname')['pageStartPosition'].    agg(['count', 'mean']).reset_index()
@@ -66,18 +66,22 @@ df_plot.columns = ['cname', 'weeks', 'position']
 df_plot =     df_plot[df_plot['weeks'] >= MIN_WEEKS].reset_index(drop=True)
 
 
-# In[65]:
+# In[22]:
 
 
 fig = px.scatter(
     df_plot, x='position', y='weeks', opacity=0.7, 
     hover_data=['cname'], title='作品別の平均掲載位置と連載週数')
+fig.update_traces(
+    marker={'size': 10, 'line_width':1,})
+fig.update_xaxes(title='平均掲載位置')
+fig.update_yaxes(title='連載週数')
 show_fig(fig)
 
 
 # ### 雑誌別・作品別の平均掲載位置と連載週数
 
-# In[66]:
+# In[23]:
 
 
 df_plot =     df.groupby(['mcname', 'cname'])['pageStartPosition'].    agg(['count', 'mean']).reset_index()
@@ -85,20 +89,25 @@ df_plot.columns = ['mcname', 'cname', 'weeks', 'position']
 df_plot =     df_plot[df_plot['weeks'] >= MIN_WEEKS].reset_index(drop=True)
 
 
-# In[67]:
+# In[24]:
 
 
 fig = px.scatter(
     df_plot, x='position', y='weeks', color='mcname', 
     opacity=0.7,
     hover_data=['cname'], 
+    color_discrete_sequence= px.colors.diverging.Portland,
     title='雑誌別・作品別の平均掲載位置と連載週数')
+fig.update_traces(
+    marker={'size': 10, 'line_width':1})
+fig.update_xaxes(title='平均掲載位置')
+fig.update_yaxes(title='連載週数')
 show_fig(fig)
 
 
 # ### 雑誌別・作品別の平均掲載位置と連載週数と平均ページ数
 
-# In[68]:
+# In[25]:
 
 
 df_plot =     df.groupby(['mcname', 'cname'])    [['pages', 'pageStartPosition']].    agg(['count', 'mean']).reset_index()
@@ -108,18 +117,18 @@ df_plot.columns = [
 df_plot =     df_plot[df_plot['weeks'] >= MIN_WEEKS].reset_index(drop=True)
 
 
-# In[69]:
+# In[26]:
 
 
 fig = px.scatter(
     df_plot, x='position', y='weeks', color='mcname',
     size='pages', opacity=0.7,
+    color_discrete_sequence= px.colors.diverging.Portland,
     hover_data=['cname'], title='雑誌別・作品別の平均掲載位置と連載週数')
+fig.update_traces(
+    marker={'line_width':1})
+fig.update_xaxes(title='平均掲載位置')
+fig.update_yaxes(title='連載週数')
+
 show_fig(fig)
-
-
-# In[ ]:
-
-
-
 
