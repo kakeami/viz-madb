e78b0637e9e67e2950abd2d3933554c1b6265f81
kakeami <kakeami.contact@gmail.com>
2022-01-29 15:24:01 +0900

    パラレルセットグラフまで修正完了

--- book/_build/jupyter_execute/charts4props/bars.py
+++ book/_build/jupyter_execute/charts4props/bars.py
@@ -5,8 +5,8 @@
 
 # ## 概要
 
-# 質的変数の比率を見る際にも棒グラフは有用です．
-# 縦横反転した[積上げ棒グラフ](https://kakeami.github.io/viz-madb/charts4amounts/bars.html#id4)がよく用いられます．
+# 質的変数の比率を見る際にも **棒グラフ** は有用です．
+# この用途でよく用いられるのは，縦横反転した[積上げ棒グラフ](https://kakeami.github.io/viz-madb/charts4amounts/bars.html#id4)です．
 
 # ![](../figs/charts/bars_prop.png)
 
--- book/_build/jupyter_execute/charts4props/mosaic.py
+++ book/_build/jupyter_execute/charts4props/mosaic.py
@@ -5,8 +5,8 @@
 
 # ## 概要
 
-# **モザイクプロット（Mosaic Plot）**とは，複数の質的変数に対して，その比率を**四角形の面積**で表したグラフです．
-# その見た目から**マリメッコプロット（Marimekko Plot）**とも呼ばれます．
+# **モザイクプロット（Mosaic Plot）** とは，複数の質的変数に対して，その比率を **長方形の面積** で表したグラフです．
+# その見た目から **マリメッコプロット（Marimekko Plot）** とも呼ばれます．
 # 分割方法を工夫することで三変数以上にも対応可能ですが，私がよく見るのは二変数に対する描画です．
 # 
 # 二変数に対するモザイクプロットは，[積上げ棒グラフ](https://kakeami.github.io/viz-madb/charts4props/bars.html)の棒の太さを，分母の大きさで調整したものと捉えることができます．
@@ -19,25 +19,15 @@
 
 # ## Plotlyによる作図方法
 
-# Plotlyで直接モザイクプロットを描画する方法はありません．[こちら](https://plotly.com/python/bar-charts/)を参考に，棒グラフを応用して作図します．
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+# Plotlyで直接モザイクプロットを描画する方法はありません．
+# [こちら](https://plotly.com/python/bar-charts/)を参考に，棒グラフを応用して作図します．
+# 非常に複雑なので，以下の作図例にコメントを入れる形で解説します．
 
 # ## MADB Labを用いた作図例
 
 # ### 下準備
 
-# In[2]:
+# In[1]:
 
 
 import pandas as pd
@@ -48,7 +38,7 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
-# In[3]:
+# In[2]:
 
 
 # 前処理の結果，以下に分析対象ファイルが格納されていることを想定
@@ -57,7 +47,7 @@ PATH_DATA = '../../data/preprocess/out/episodes.csv'
 RENDERER = 'plotly_mimetype+notebook'
 
 
-# In[4]:
+# In[3]:
 
 
 def add_years_to_df(df, unit_years=10):
@@ -68,7 +58,7 @@ def add_years_to_df(df, unit_years=10):
     return df_new
 
 
-# In[5]:
+# In[4]:
 
 
 def show_fig(fig):
@@ -77,7 +67,7 @@ def show_fig(fig):
     fig.show(renderer=RENDERER)
 
 
-# In[6]:
+# In[5]:
 
 
 df = pd.read_csv(PATH_DATA)
@@ -85,10 +75,60 @@ df = pd.read_csv(PATH_DATA)
 
 # ### 雑誌別・年代別の合計作品数
 
-# In[7]:
+# In[6]:
 
 
 col_count = 'cname'
+
+
+# In[7]:
+
+
+# 10年単位で区切ったyearsを追加
+df = add_years_to_df(df, 10)
+# mcname, yearsで集計
+df_plot =     df.groupby(['mcname', 'years'])[col_count].    nunique().reset_index()
+# years単位で集計してdf_plotにカラムを追加
+df_tmp = df_plot.groupby('years')[col_count].sum().reset_index(
+    name='years_total')
+df_plot = pd.merge(df_plot, df_tmp, how='left', on='years')
+# years合計あたりの比率を計算
+# 棒グラフの太さを調整する際に利用する
+df_plot['ratio'] = df_plot[col_count] / df_plot['years_total']
+
+
+# In[8]:
+
+
+fig = go.Figure()
+for mcname in df_plot['mcname'].unique():
+    df_tmp =         df_plot[df_plot['mcname']==mcname].reset_index(drop=True)
+    widths = df_tmp['years_total']
+    fig.add_trace(go.Bar(
+        name=mcname,
+        # X軸方向の位置を，棒グラフの太さを考慮して調整
+        x=df_tmp['years_total'].cumsum() - widths,
+        y=df_tmp['ratio'], text=df_tmp[col_count],
+        # 棒グラフの太さを調整
+        width=widths,
+        offset=0,))
+fig.update_xaxes(
+    # X軸の目盛りの位置を，棒グラフの太さを考慮して調整
+    tickvals=widths.cumsum() - widths/2,
+    ticktext=df_plot['years'].unique(),)
+fig.update_xaxes(title='期間')
+fig.update_yaxes(title='比率')
+# go.Barを積み上げる設定
+fig.update_layout(barmode='stack', title_text='雑誌別・年代別の合計作品数')
+show_fig(fig)    
+
+
+# ### 雑誌別・年代別の合計作家数
+
+# In[11]:
+
+
+col_count = 'creator'
 
 
 # In[12]:
@@ -103,10 +143,11 @@ df_tmp = df_plot.groupby('years')[col_count].sum().reset_index(
     name='years_total')
 df_plot = pd.merge(df_plot, df_tmp, how='left', on='years')
 # years合計あたりの比率を計算
+# 棒グラフの太さを調整する際に利用
 df_plot['ratio'] = df_plot[col_count] / df_plot['years_total']
 
 
-# In[15]:
+# In[13]:
 
 
 fig = go.Figure()
@@ -115,60 +156,19 @@ for mcname in df_plot['mcname'].unique():
     widths = df_tmp['years_total']
     fig.add_trace(go.Bar(
         name=mcname,
+        # 棒グラフの太さを考慮してX軸方向の位置を調整
         x=df_tmp['years_total'].cumsum() - widths,
         y=df_tmp['ratio'], text=df_tmp[col_count],
+        # 棒グラフの太さを調整
         width=widths,
         offset=0,))
 fig.update_xaxes(
+    # 棒グラフの太さを考慮してX軸の目盛りの位置を調整
     tickvals=widths.cumsum() - widths/2,
     ticktext=df_plot['years'].unique(),)
 fig.update_xaxes(title='期間')
 fig.update_yaxes(title='比率')
-fig.update_layout(barmode='stack', title_text='雑誌別・年代別の合計作品数')
-show_fig(fig)    
-
-
-# ### 雑誌別・年代別の合計作家数
-
-# In[20]:
-
-
-col_count = 'creator'
-
-
-# In[21]:
-
-
-# 10年単位で区切ったyearsを追加
-df = add_years_to_df(df, 10)
-# mcname, yearsで集計
-df_plot =     df.groupby(['mcname', 'years'])[col_count].    nunique().reset_index()
-# years単位で集計してdf_plotにカラムを追加
-df_tmp = df_plot.groupby('years')[col_count].sum().reset_index(
-    name='years_total')
-df_plot = pd.merge(df_plot, df_tmp, how='left', on='years')
-# years合計あたりの比率を計算
-df_plot['ratio'] = df_plot[col_count] / df_plot['years_total']
-
-
-# In[23]:
-
-
-fig = go.Figure()
-for mcname in df_plot['mcname'].unique():
-    df_tmp =         df_plot[df_plot['mcname']==mcname].reset_index(drop=True)
-    widths = df_tmp['years_total']
-    fig.add_trace(go.Bar(
-        name=mcname,
-        x=df_tmp['years_total'].cumsum() - widths,
-        y=df_tmp['ratio'], text=df_tmp[col_count],
-        width=widths,
-        offset=0,))
-fig.update_xaxes(
-    tickvals=widths.cumsum() - widths/2,
-    ticktext=df_plot['years'].unique(),)
-fig.update_xaxes(title='期間')
-fig.update_yaxes(title='比率')
+# go.Bar()を積み上げる設定
 fig.update_layout(
     barmode='stack', title_text='雑誌別・年代別の合計作家数')
 show_fig(fig)    
